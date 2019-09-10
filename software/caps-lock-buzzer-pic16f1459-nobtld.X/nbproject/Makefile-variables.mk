aa52046166e43f04bc2130e70969f3c125ed74f3
bikerglen <glen@DESKTOP-BEAST.localdomain>
2019-09-09 21:26:51 -0600

    added boards, enclosure, and software

---
+++ software/caps-lock-buzzer-pic16f1459-nobtld.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# LPCUSBDK_16F1459 configuration
+CND_ARTIFACT_DIR_LPCUSBDK_16F1459=dist/LPCUSBDK_16F1459/production
+CND_ARTIFACT_NAME_LPCUSBDK_16F1459=caps-lock-buzzer-pic16f1459-nobtld.X.production.hex
+CND_ARTIFACT_PATH_LPCUSBDK_16F1459=dist/LPCUSBDK_16F1459/production/caps-lock-buzzer-pic16f1459-nobtld.X.production.hex
+CND_PACKAGE_DIR_LPCUSBDK_16F1459=${CND_DISTDIR}/LPCUSBDK_16F1459/package
+CND_PACKAGE_NAME_LPCUSBDK_16F1459=caps-lock-buzzer-pic16f1459-nobtld.x.tar
+CND_PACKAGE_PATH_LPCUSBDK_16F1459=${CND_DISTDIR}/LPCUSBDK_16F1459/package/caps-lock-buzzer-pic16f1459-nobtld.x.tar
